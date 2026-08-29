3689005b91792069ca3c8973ab817832a07a53a7
Alok <arhalok@gmail.com>
2026-08-29 18:55:42 +0000

    Clean duplicate root Python files

--- profit.py
+++
@@ -1,2 +0,0 @@
-def calculate_transport_cost(distance_km, cost_per_km, loading_cost=0, unloading_cost=0, toll=0):
-    return round(distance_km * cost_per_km + loading_cost + unloading_cost + toll, 2)
